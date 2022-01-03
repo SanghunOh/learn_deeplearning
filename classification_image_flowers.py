10864af9fede441a208c326c22f6a9fe9e22320e
Sanghun Oh <otter.oh@gmail.com>
2022-01-03 18:30:06 +0900

    image flowers

---
+++ classification_image_flowers.py
@@ -0,0 +1,12 @@
+# refer : https://www.tensorflow.org/tutorials/images/classification
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
